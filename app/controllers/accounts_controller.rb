c5054384bf6143f9d841ec4c14b037d3b33488a3
boutayna badou <boutayna.badou@gmail.com>
2015-06-23 16:13:03 +0200

    test

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,18 @@
+class AccountsController < ApplicationController
+  before_action :set_user
+
+  def show
+  end
+
+  def edit
+
+  end
+
+
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+end
