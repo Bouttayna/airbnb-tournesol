3f06570ed78020bcfa29d22739b62b76a6daa36f
julien mamalian <julien.mamalian@gmail.com>
2015-06-25 15:18:53 +0200

    modale

--- app/assets/javascripts/profile_user.js
+++ app/assets/javascripts/profile_user.js
@@ -4,14 +4,14 @@ $(document).ready(function(){
 
   $('#link_locataires').on('click', function(event){
     $(this).addClass("selected");
-    $('#locataires_details').show(1500);
-    $('#close_icon').show(1800);
+    $('#locataires_details').show(1000);
+    $('#close_icon').show(1200);
     $('#locataires_details h4').removeClass('hover');
   });
   $('#close_icon').on('click', function(event){
     $('#link_locataires').removeClass("selected");
-    $('#locataires_details').hide(1500);
-    $(this).hide(2000);
+    $('#locataires_details').hide(1200);
+    $(this).hide(1400);
   });
 
   // $('#account_link').on('click', function(){
