504b362a0e23b4da16c3353f84048eee8ccab852
boutayna badou <boutayna.badou@gmail.com>
2015-06-25 12:57:43 +0200

    removed column picture from user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150624123301) do
+ActiveRecord::Schema.define(version: 20150625104716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,9 +67,12 @@ ActiveRecord::Schema.define(version: 20150624123301) do
     t.text     "description"
     t.string   "provider"
     t.string   "uid"
-    t.string   "picture"
     t.string   "token"
     t.datetime "token_expiry"
+    t.string   "picture_file_name"
+    t.string   "picture_content_type"
+    t.integer  "picture_file_size"
+    t.datetime "picture_updated_at"
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
